fdd59ead2bd0ab8642a6054e3f1392f428fd25c0
pyj <yepark98@gmail.com>
2023-03-14 14:48:48 +0900

    글 가져오기 구현

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,36 +1,14 @@
 import React from "react";
 import styles from "./ListItem.module.css";
 
-const ListItem = () => {
+const ListItem = (props) => {
+  const { post } = props;
+
   return (
     <>
       <li className={styles.listItem}>
-        <h3 className={styles.title}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-          reiciendis animi, magni tempore ipsam labore nemo corporis distinctio
-          illum excepturi debitis, id hic autem laborum deserunt dolorem vitae
-          accusamus possimus!
-        </h3>
-        <p className={styles.content}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-          reiciendis animi, magni tempore ipsam labore nemo corporis distinctio
-          illum excepturi debitis, id hic autem laborum deserunt dolorem vitae
-          accusamus possimus!
-        </p>
-      </li>
-      <li className={styles.listItem}>
-        <h3 className={styles.title}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-          reiciendis animi, magni tempore ipsam labore nemo corporis distinctio
-          illum excepturi debitis, id hic autem laborum deserunt dolorem vitae
-          accusamus possimus!
-        </h3>
-        <p className={styles.content}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-          reiciendis animi, magni tempore ipsam labore nemo corporis distinctio
-          illum excepturi debitis, id hic autem laborum deserunt dolorem vitae
-          accusamus possimus!
-        </p>
+        <h3 className={styles.title}>{post.title}</h3>
+        <p className={styles.content}>{post.content}</p>
       </li>
     </>
   );
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./MainPage.module.css";
+import axios from "axios";
 
 import SearchBar from "./SearchBar";
 import ListItem from "./ListItem";
 
 const MainPage = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios.get("http://localhost:3001/posts");
+      return result.data;
+    }
+    fetchData().then((res) => {
+      setPosts(res);
+    });
+  }, []);
+
   return (
     <section className={styles.container}>
       <SearchBar />
       <ul className={styles.list}>
-        <ListItem />
+        {posts.map((post) => (
+          <ListItem key={post.id} post={post} />
+        ))}
       </ul>
     </section>
   );
