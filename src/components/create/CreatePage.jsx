08e8dfb5b9e6f503adca0fa3801187e4f4bcdc69
pyj <yepark98@gmail.com>
2023-03-14 14:44:41 +0900

    글 작성 구현

--- src/components/create/CreatePage.jsx
+++ src/components/create/CreatePage.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import cx from "clsx";
 import styles from "./CreatePage.module.css";
 
 const CreatePage = () => {
+  const [text, setText] = useState({ title: "", content: "" });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setText({ ...text, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios.post("http://localhost:3001/posts", {
+      title: text.title,
+      content: text.content,
+    });
+    navigate("/");
+  };
+
+  console.log(text);
+
   return (
     <section className={styles.container}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className={styles.buttonArea}>
           <button type="submit" className={styles.button}>
             글 등록
@@ -17,6 +40,8 @@ const CreatePage = () => {
             name="title"
             required
             placeholder="제목"
+            value={text.title}
+            onChange={handleChange}
             className={cx(styles.input, styles.border)}
           />
 
@@ -24,6 +49,8 @@ const CreatePage = () => {
             name="content"
             required
             placeholder="내용을 입력하세요."
+            value={text.content}
+            onChange={handleChange}
             className={cx(styles.textarea, styles.border)}
           />
         </div>
